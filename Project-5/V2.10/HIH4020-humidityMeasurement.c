eb81d1c4591bcb7c00b74e06f586e522f8874acd
Florian Wilhelm Dirnberger <Florian.dirnberger@ome-eng.de>
2026-02-24 08:28:09 +0100

    Update HIH4020-humidityMeasurement.c

--- Project-5/V2.10/HIH4020-humidityMeasurement.c
+++ Project-5/V2.10/HIH4020-humidityMeasurement.c
@@ -212,7 +212,7 @@ int startServerWiFi(uint8_t buf[], int len, int WDindicator)
         adc_select_input(0); // ADC0 on Pin 31
         
         uint16_t rawRHSensor = adc_read(); // raw value (max 4095)
-        float physicalRHSensor = ((((rawRHSensor-815)/2.6)*3.3)/4095)*100; // maths for the RH sensor HIH4020 (including an offset), essentially for 25°C; voltage divider with 10k+10k and 100k on sensor output; note: sensor is ratiometric to supply voltage       
+        float physicalRHSensor = ((((rawRHSensor-815)/2.6)*3.3)/4095)*100; // maths for the RH sensor HIH4020 (including an offset), feel free to question it; voltage divider with 10k+10k and 100k on the sensor output in order not to exceed 3.3V; note: sensor is ratiometric to supply voltage       
         uint16_t physicalRHSensorT = (uint16_t)(physicalRHSensor/(1.0546-0.00216*physicalTempSensor)); // temperature compensation, see data sheet
         
         adc_select_input(2); // ADC2 on Pin 34
